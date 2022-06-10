b37479e6ef4c577481a7f4306107ad77effd082a
Christian <christian.2994@hotmail.com>
2022-06-10 00:01:00 -0300

    assert statement class

--- assert_statement.py
+++ assert_statement.py
@@ -7,10 +7,19 @@ def divisors(num):
 
 def run():
         num = input("Ingresa un numero: ")      #Elimine la funcion int() porque isnumeric() solo puede evaluar strings
-        assert num.isnumeric(), "Debes ingresar un numero"   #Metodo de los strings que devuelve true o false si el string es un tipo de numero
-        print(divisors(int(num)))
+        assert num.isnumeric(), "Debes ingresar un numero"   #Metodo de los strings que devuelve true o
+        print(divisors(int(num)))                            # false si el string es un tipo de numero
         print("Termino mi programa")
 
 if __name__ == '__main__':          #Entry Point
     run()
     
+#Con este codigo pido un numero al usuario, y con num.isnumeric() me fijo si es un numero
+#Si no lo es, salta una excepcion con el mensaje que puse
+
+#Alternativa
+def run():
+        num = input("Ingresa un numero: ")     
+        assert num.isnumeric() and int(num) > 0 , "Debes ingresar solo numeros positivos"   
+        print(divisors(int(num)))                            
+        print("Termino mi programa")
